4808b613aa1989741ef1a58d949841b79e661f41
himanshukumar76 <himanshukumar75655893@gmail.com>
2024-10-30 18:11:04 +0530

    file commited successfully

---
+++ UPSKILLMAFIA/LEARN JS/day 1/intro-app.js
@@ -0,0 +1,21 @@
+console.log("Himanshu kumar");
+
+let myName = "Himanshu";
+myName = "Sundaram";
+console.log(myName);
+
+// const
+const pi = 3.14;
+console.log(pi);
+// scoping
+{
+    let name1 = "Abhi"; 
+    console.log("Scope : ", name1); 
+}
+
+// data conversion
+let age = 18; 
+age = "good"; 
+console.log(age); 
+
+// nothing
---
+++ UPSKILLMAFIA/LEARN JS/day 4/reference data object memory/app.js
@@ -0,0 +1,17 @@
+// MEMORY MANAGEMENT
+
+let a = 10; 
+let b = a;
+b=20; 
+console.log(a); 
+console.log(b); 
+
+// reference data type 
+// object : collection of key:value pair
+
+const person = {
+    name:"John",
+    age:24 
+}
+
+console.log(person); 
