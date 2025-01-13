7558b8e33e303e710fa0bb2261687d96b59518a7
himanshukumar76 <himanshukumar75655893@gmail.com>
2025-01-13 10:52:34 +0530

    some new files added and modified and some existing some also modified

--- LEARN JAVASCRIPT/JAVA SCRIPT BY PRASHANT/PROJECTS/CRICKET_GAME/CRICKET_GAME IMPROVED USING OBJECTS/cricket.js
+++ LEARN JAVASCRIPT/JAVA SCRIPT BY PRASHANT/PROJECTS/CRICKET_GAME/CRICKET_GAME IMPROVED USING OBJECTS/cricket.js
@@ -1,3 +1,13 @@
+let score = {
+  win:0,
+  lost:0,
+  tie:0,
+  displayScore: function(){
+    return `No of matches Win:${score.win}   Lost: ${score.lost}    Tie: ${score.tie}`; 
+  }
+}
+
+
 function generateComputerChoice() {
   let computerChoice;
   let randomNumber = Math.random() * 3;
@@ -18,14 +28,22 @@ function generateComputerChoice() {
 function getResult(userMove, computerMove) {
 
   if (computerMove === userMove) {
+    score.tie++; 
     return `It's a draw!`;
   } else if ((computerMove === 'Bat' && userMove === 'Ball') || (computerMove === 'Ball' && userMove === 'Stump') || (computerMove === 'Stump' && userMove === 'Bat')) {
+    score.lost++; 
     return 'Computer has won !!'; 
   } else {
+    score.win++; 
     return "User has won!";
   }
 }
 
 function showResult(userMove, computerMove, result){   
-  alert(`User choice is ${userMove}. and Computer choice is ${computerMove} and the result is ${result}`)
+  // console.log(score);
+  alert(`User choice is ${userMove}. and Computer choice is ${computerMove} and
+
+     the result is ${result}
+     
+     ${score.displayScore()}`)
 }
