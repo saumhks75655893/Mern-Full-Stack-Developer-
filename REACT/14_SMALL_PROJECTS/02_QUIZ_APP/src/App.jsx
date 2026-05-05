310371ba7ad77d2b7cfbd1afdaacc2f0b0ba16b7
himanshukumar76 <himanshukumar75655893@gmail.com>
2026-05-05 17:31:33 +0530

    js

---
+++ REACT/14_SMALL_PROJECTS/02_QUIZ_APP/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <button
+        className="p-4  m-34 text-4xl bg-red-500 rounded-2xl text-white font-bold border-4
+       "
+      >
+        Code
+      </button>
+    </div>
+  );
+};
+
+export default App;
