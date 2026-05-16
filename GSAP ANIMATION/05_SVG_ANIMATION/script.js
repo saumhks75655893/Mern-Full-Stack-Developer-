8eedf1e1a5c286024bde8245f61f6e1908a0e4f8
himanshukumar76 <himanshukumar75655893@gmail.com>
2026-05-16 18:03:15 +0530

    string bouncing animation using dom and easing

--- GSAP ANIMATION/05_SVG_ANIMATION/script.js
+++ GSAP ANIMATION/05_SVG_ANIMATION/script.js
@@ -1,3 +1,21 @@
-const initialPath = `M 10 250 Q 800 500 1590 250`;
+let initialPath = "M 15 250 Q 625 250 1250 250";
 
-const finalPath = `M 10 250 Q 800 500 1590 250`;
+let finalPath = `M 15 250 Q 625 250 1250 250`;
+
+const string = document.querySelector("#string");
+string.addEventListener("mousemove", (e) => {
+  initialPath = `M 15 250 Q ${e.x} ${e.y} 1250 250`;
+  gsap.to("svg path", {
+    attr: { d: initialPath },
+    duraton: 0.2,
+    ease: "power3.out",
+  });
+});
+
+string.addEventListener("mouseleave", () => {
+  gsap.to("svg path", {
+    attr: { d: finalPath },
+    duration: 1.8,
+    ease: "elastic.out(1, 0.2)",
+  });
+});
