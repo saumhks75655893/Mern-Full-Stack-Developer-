98766b0b7f63fd5f3b8121facd90746d2ca66f04
himanshukumar76 <himanshukumar75655893@gmail.com>
2026-06-01 13:33:23 +0530

    virtualization

--- REACT/41_PERFORMANCE_OPTIMIZATION_THROUGH_PROJECT/src/App.jsx
+++ REACT/41_PERFORMANCE_OPTIMIZATION_THROUGH_PROJECT/src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import { Routes, Route, } from 'react-router-dom'
+import React, { lazy } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import Dashboard from './components/Dashboard'
 import About from './components/About'
 import Settings from './components/Settings'
-import { Routes, Route } from 'react-router-dom'
+
+
+const home = lazy(() => import("./components/Home.jsx"))
+const dashboard = lazy(() => import("./components/Dashboard.jsx"))
+const about = lazy(() => import("./components/About.jsx"))
+const settings = lazy(() => import("./components/Settings.jsx"))
 
 const App = () => {
   return (
     <div className='flex flex-col min-h-screen'>
 
-      <Navbar className="sticky top-0" />
+      <Navbar className="sticky top-0 z-500" />
 
       <div className='flex flex-col items-center justify-center flex-1'>
         <Routes>
--- REACT/41_PERFORMANCE_OPTIMIZATION_THROUGH_PROJECT/src/components/Home.jsx
+++ REACT/41_PERFORMANCE_OPTIMIZATION_THROUGH_PROJECT/src/components/Home.jsx
@@ -1,43 +1,39 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import Hero from './Hero.jsx'
+
+// LAZY import -> component based spilting
+const VideoSection = lazy(() => import("./VideoSection.jsx"))
+const Settings = lazy(() => import("./Settings.jsx"))
+const Testimonials = lazy(() => import("./Testimonials.jsx"))
+const Courses = lazy(() => import("./Courses.jsx"))
+const FAQ = lazy(() => import("./FAQ.jsx"))
 
 const Home = () => {
   return (
     <div className='uppercase font-bold text-center'>
 
-      <section className="hero">
-        <h1>Build Amazing Websites</h1>
-        <p>
-          Modern, Responsive and Attractive React Website Design
-        </p>
+      <Hero />
 
-        <button>Get Started</button>
-      </section>
+      <Suspense fallback="Loading...">
+        <Settings />
+      </Suspense>
 
+      <Suspense fallback="Loading...">
+        <Testimonials />
+      </Suspense>
 
-      < h1 className='text-7xl text-blue-500 p-4'> Welcome to My Website</h1 >
-      <p className='text-3xl text-black-800 bg-blue-600 p-7 rounded-2xl'>
-        This is the Home Page. Here you can add your main content, banner,
-        features, and other information.
-      </p>
+      <Suspense fallback="Loading...">
+        <Courses />
+      </Suspense>
 
+      <Suspense fallback="Loading...">
+        <VideoSection />
+      </Suspense>
 
+      <Suspense fallback="Loading...">
+        <FAQ />
+      </Suspense>
 
-      <section className="cards">
-        <div className="card">
-          <h3>Fast</h3>
-          <p>Optimized performance and speed.</p>
-        </div>
-
-        <div className="card">
-          <h3>Responsive</h3>
-          <p>Works on Mobile, Tablet and Desktop.</p>
-        </div>
-
-        <div className="card">
-          <h3>Modern UI</h3>
-          <p>Clean and attractive user interface.</p>
-        </div>
-      </section>
     </div>
 
   )
